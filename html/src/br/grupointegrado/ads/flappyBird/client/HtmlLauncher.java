6c6b847288763c66155be19005c46959994c719e
Rafael Monteiro do Nascimento <rafaelmonteiro200@gmail.com>
2015-09-28 19:52:32 -0300

    Criacao Projeto

---
+++ html/src/br/grupointegrado/ads/flappyBird/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package br.grupointegrado.ads.flappyBird.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import br.grupointegrado.ads.flappyBird.MainGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MainGame();
+        }
+}
